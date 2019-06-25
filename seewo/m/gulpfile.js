8267250dea16042aba793a79791e3a4a39cb8b1c
Luzhijian <739788147@qq.com>
2019-06-25 16:07:40 +0800

    update

--- seewo/m/gulpfile.js
+++ seewo/m/gulpfile.js
@@ -81,12 +81,12 @@ gulp.task('testImagemin', function () {
       hasChanged: changed.compareSha1Digest
     }))
     .pipe(plumber())
-    .pipe(imagemin({
-      optimizationLevel: 3, //类型：Number  默认：3  取值范围：0-7（优化等级）
-      progressive: true, //类型：Boolean 默认：false 无损压缩jpg图片
-      interlaced: true, //类型：Boolean 默认：false 隔行扫描gif进行渲染
-      multipass: true //类型：Boolean 默认：false 多次优化svg直到完全优化
-    }))
+    // .pipe(imagemin({
+    //   optimizationLevel: 3, //类型：Number  默认：3  取值范围：0-7（优化等级）
+    //   progressive: true, //类型：Boolean 默认：false 无损压缩jpg图片
+    //   interlaced: true, //类型：Boolean 默认：false 隔行扫描gif进行渲染
+    //   multipass: true //类型：Boolean 默认：false 多次优化svg直到完全优化
+    // }))
     .pipe(gulp.dest('dev/images'));
     // .pipe(browserSync.reload({
     //   stream: true
